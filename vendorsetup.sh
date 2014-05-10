59a92c071c7b1977eb465fff6f19c6209e84a1b1
Olivier <y.karasangabo@bg-rams.ac.at>
2014-05-10 11:53:00 +0200

    marvel: fix vendorsetup for aosp/b2g

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -20,5 +20,7 @@
 # In particular, you can add lunch options with the add_lunch_combo
 # function: add_lunch_combo generic-eng
 
+add_lunch_combo full_marvel-userdebug
+add_lunch_combo full_marvel-eng
 add_lunch_combo cm_marvel-userdebug
 add_lunch_combo cm_marvel-eng
