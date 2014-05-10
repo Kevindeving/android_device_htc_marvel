39cc6d431d92d82b4bdae9d95b5960873c34139c
Olivier <y.karasangabo@bg-rams.ac.at>
2014-05-10 12:00:28 +0200

    marvel: cleanup and fix build

--- cm.mk
+++ cm.mk
@@ -1,5 +1,3 @@
-PRODUCT_RELEASE_NAME := CRYPTOMILK
-
 # Boot animation
 TARGET_BOOTANIMATION_NAME := vertical-320x480
 
@@ -8,19 +6,10 @@ $(call inherit-product, device/htc/marvel/device_marvel.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := marvel
-PRODUCT_NAME := cm_marvel
+PRODUCT_NAME := full_marvel
 PRODUCT_BRAND := htc_europe
 PRODUCT_MODEL := Wildfire S A510e
 PRODUCT_MANUFACTURER := HTC
 
-PRODUCT_VERSION_DEVICE_RELEASE := ALPHA7
-PRODUCT_VERSION_DEVICE_SPECIFIC := -CRYPTOMILK-$(PRODUCT_VERSION_DEVICE_RELEASE)
-
-# CM_RELEASE := true
-CM_BUILDTYPE := UNOFFICIAL
-CM_EXTRAVERSION := CRYPTOMILK-$(PRODUCT_VERSION_DEVICE_RELEASE)
-
-BUILD_ID := ASN00$(shell date -u +%Y%m%d)
-
 # Set build fingerprint / ID / Product Name ect.
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=htc_marvel BUILD_ID=$(BUILD_ID) BUILD_DISPLAY_ID=$(BUILD_ID) BUILD_FINGERPRINT=cyanogenmod/htc_marvel/marvel:4.0.4/ASN$(BUILD_ID)/0.1:user/release-keys PRIVATE_BUILD_DESC="0.1 CRYPTOMILK release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=htc_marvel BUILD_ID=$(BUILD_ID) BUILD_DISPLAY_ID=$(BUILD_ID) BUILD_FINGERPRINT=cyanogenmod/htc_marvel/marvel:4.0.4/$(BUILD_ID)/0.1:user/release-keys PRIVATE_BUILD_DESC="release-keys"
